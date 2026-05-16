f3a30ba3dcc74986da4df655ee2675bea119eefa
Mariano Van Staden <marianovanstaden@gmail.com>
2026-05-16 10:24:17 -0300

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -6,7 +6,7 @@ import tseslint from 'typescript-eslint'
 import { globalIgnores } from 'eslint/config'
 
 export default tseslint.config([
-  globalIgnores(['dist']),
+  globalIgnores(['dist', 'coverage', '*.cjs', 'scripts/**/*.cjs']),
   {
     files: ['**/*.{ts,tsx}'],
     extends: [
